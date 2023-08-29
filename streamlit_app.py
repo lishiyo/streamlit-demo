cd1456ab505a91a8ab7d6de59d560120afb46509
Connie Li <connieli@tumblr.com>
2023-08-28 21:13:30 -0700

    add https://blog.streamlit.io/langchain-tutorial-1-build-an-llm-powered-app-in-18-lines-of-code/

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,19 @@
 import streamlit as st
+from langchain.llms import OpenAI
 
-st.title('🎈 App Name')
+st.title('🦜🔗 Quickstart App')
 
-st.write('Hello world!')
+openai_api_key = st.sidebar.text_input('OpenAI API Key')
+
+def generate_response(input_text, temperature=0.7, max_tokens=-1, top_p=1.0, frequency_penalty=0.0, presence_penalty=0.0, stop=['\n']):
+  # https://api.python.langchain.com/en/latest/llms/langchain.llms.openai.OpenAI.html
+  llm = OpenAI(temperature, openai_api_key=openai_api_key)
+  st.info(llm(input_text))
+
+with st.form('my_form'):
+  text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
+  submitted = st.form_submit_button('Submit')
+  if not openai_api_key.startswith('sk-'):
+    st.warning('Please enter your OpenAI API key!', icon='⚠')
+  if submitted and openai_api_key.startswith('sk-'):
+    generate_response(text)
